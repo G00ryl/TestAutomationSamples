939aec27826dc493894166cd268612cbc86899c6
G00ryl <dawidgorla@interia.pl>
2021-03-22 21:57:08 +0100

    Still fighting

--- src/main/java/utils/WindowManager.java
+++ src/main/java/utils/WindowManager.java
@@ -36,7 +36,6 @@ public class WindowManager {
         System.out.println("Number of tabs: " +windows.size());
 
         System.out.println("Window handles: ");
-        windows.forEach(System.out::println);
 
         for(String window : windows){
             System.out.println("Switching to window: " + window);
